143f9e9efcbe5ad5372672086a5d30981c9c7abc
seulle <seull@LAPTOP-L9AOK14Q.>
2024-01-31 02:15:27 +0900

    100분이상 출력

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <script setup>
 
+import { ref } from 'vue'
+import category from "@/assets/gamelist.json"
 
-import category from "@/assets/category.json"
+const categories = ref(category)
+const awesome = ref(true)
 
-const categories = category
+
 </script>
 
 <template>
 
+<h1>게임플레이타임 100분이상 출력하기</h1>
 
-<h1 v-for="item in categories">
-  name : {{ item.name }}  
-  code : {{ item.code }}
- </h1>
+<dev v-for="item in categories">
+<li v-if="item.playtime_forever > 100">
+게임이름 : {{ item.name }} <br>
+  나의 플레이 시간 : {{item.playtime_forever}} 분
+</li>
+</dev>
 </template>
