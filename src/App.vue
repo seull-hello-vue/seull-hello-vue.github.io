8ad66054e47b3bac77e1ae6e3982de5b152ed9db
seulle <seull@LAPTOP-L9AOK14Q.>
2024-01-30 19:28:36 +0900

    app.vue 수정1

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
 <script setup>
 import { ref } from 'vew'
-const 
+const count = ref(0)
+</script>
+
+<template>
+<button @click="count++">Count is : {{ count }}</button>
+</template>
+
+<style scoped>
+button{
+font-weight: blod;
+}
+</style>
