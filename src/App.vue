72aa53a4a89b176bbb76fa7969da3cf421e075bb
seulle <seull@LAPTOP-L9AOK14Q.>
2024-01-30 20:40:20 +0900

    count up and down

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
 <script setup>
-import { ref } from 'vew'
+import { ref,onMounted } from 'vue'
+
+//reactive state
 const count = ref(0)
+
+function increment(){
+count.value = count.value+1
+//count.value++
+}
+
+ function increment1(){
+   count.value--
+  }
+
+//lifecycle hooks
+onMounted(() => {
+console.log('The initial count is ${count.value}.')
+})
+
 </script>
 
 <template>
-<button @click="count++">Count is : {{ count }}</button>
+<button @click="increment">UP</button>
+<button @click="increment">Count is : {{ count }}</button>
+<button @click="increment1">Down</button>
 </template>
-
 <style scoped>
 button{
 font-weight: blod;
+width: 100px;
+height : 50px;
+background : yellow;
 }
 </style>
