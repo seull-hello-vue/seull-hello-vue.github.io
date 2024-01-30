ebf65e3d066a70c7b329c191ff22898734881bd3
seulle <seull@LAPTOP-L9AOK14Q.>
2024-01-30 23:51:54 +0900

    상하차..

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
-import { ref } from 'vue'
 
-const parentMessage = ref('Parent')
-const items = ref([{ message: 'Foo' } , { message: 'Bar'}])
+import category from "@/assets/category.json"
+
+const categories = category
 </script>
 
 <template>
-<li v-for="(item,index) in items">
- {{ parentMessage }} - {{ index }} - {{ item.message  }}
-</li>
+
+<h1 v-for="item in categories">
+  name : {{ item.name }}  
+  code : {{ item.code }}
+ </h1>
 </template>
