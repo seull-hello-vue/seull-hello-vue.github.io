18e2430d88c8091d04890940e8db8e4d8afc9a8d
seulle <seull@LAPTOP-L9AOK14Q.>
2024-01-30 21:30:33 +0900

    if

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
 import { ref } from 'vue'
 
-const message = ref('Hello World!')
+const awesome = ref(true)
 </script>
 
 <template>
-<h1>{{ message }}</h1>
+<button @click="awesome = !awesome">toggle</button>
+
+<h1 v-if="awesome">Vue is awesome!</h1>
+<h1 v-else>oh no T_T</h1>
+<h2 v-if="awesome">true</h2>
+<h2 v-show="awesome">TRUE</h2>
 </template>
