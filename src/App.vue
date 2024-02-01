9c25faa1c7f197019f8e19d0e643b14a13dda0d1
seulle <seull@LAPTOP-L9AOK14Q.>
2024-02-01 16:09:42 +0900

    Kanban Todo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <script setup>
-import { ref, onUpdated , onMounted , onBeforeUpdate } from 'vue'
-const count = ref(0)
+import { ref } from 'vue'
 
-onUpdated(() => {
-console.log('onUpdated',document.getElementById('count').textContent)})
+let id = 0
+const newTodo = ref('')
+const todos = ref([
+	{ id: id++, issue: 'Backlog' }, { id: id++, issue: 'Todo' },
+	{ id: id++, issue: 'Going Hawaii' }
+])
 
-onMounted(() => {
-console.log('onMounted',document.getElementById('count').textContent)})
+function addTodo(){
+todos.value.push({  id: id++ ,  issue: newTodo.value })
+newTodo.value('')
+}
 
-onBeforeUpdate(() => {
-console.log('onBeforeUpdate',document.getElementById('count').textContent)})
+function removeTodo(todo){
+console.log(todo)
+todos.value = todos.value.filter((t) => t !== todo)
+}
+
+
 </script>
 
 <template>
-<button id="count" @click="count++">{{ count }}</button>
+ <form @submit.prevent="addTodo">
+  <input v-model="newTodo">
+  <button>할 일 추가</button>
+ </form>
+ <ul>
+   <li v-for="todo in todos">
+	{{ todo.issue }} - <button @click="removeTodo(todo)">Done</button>
+   </li>
+ </ul>
 </template>
